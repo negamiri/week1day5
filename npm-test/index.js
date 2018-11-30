e4bd35e7ff57b0b522b82cac07e18eef7db9c2f0
Negar Amiri <negamiri@gmail.com>
2018-11-30 20:45:41 +0000

    making my git repository work

--- npm-test/index.js
+++
@@ -1,7 +0,0 @@
-var chalk = require("chalk");
-
-var message = "Hello " + chalk.yellow("world!");
-console.log(message);
-
-var background = "Half of my message has a " + chalk.bgMagenta("magenta background");
-console.log(background);
